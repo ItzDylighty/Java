9db8f0ec83efb7c00dca7c8720824cb0d3cdd5e9
ItzDylighty <167043942+ItzDylighty@users.noreply.github.com>
2024-09-18 11:49:18 +0800

    Add files via upload

---
+++ School/src/main/java/com/mycompany/school/School.java
@@ -0,0 +1,31 @@
+package com.mycompany.school;
+
+import java.util.Scanner;
+public class School {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("Enter StudentID: ");
+        int studentID = scn.nextInt();
+        scn.nextLine();
+        System.out.print("Enter Student Name: ");
+        String name = scn.nextLine();
+        System.out.print("Enter Student Grade: ");
+        int grade = scn.nextInt();
+        scn.nextLine();
+        
+        
+        Student student = new Student(studentID, name, grade);
+        
+        student.enrollStudent();
+        student.viewGrades();
+        
+        System.out.print("Enter Update name: ");
+        String upName = scn.nextLine();
+        System.out.print("Enter Update Grades: ");
+        int upGrade = scn.nextInt();
+        student.updateInformation(upName, upGrade);
+        
+    }
+}
---
+++ School/src/main/java/com/mycompany/school/Student.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.school;
+
+public class Student {
+    int studentID;
+    String name;
+    int grade;
+    
+    public Student(int studentID, String name, int grade){
+        this.studentID = studentID;
+        this.name = name;
+        this.grade = grade;
+    }
+    public void enrollStudent(){
+        System.out.println("Enrollment System");
+        System.out.println("StudentID: " + this.studentID);
+        System.out.println("Name: " + this.name);
+        System.out.println("Grade: " + this.grade);
+
+    }
+    public void viewGrades(){
+        System.out.println("Current Grade");
+        System.out.println("Grade: " + this.grade);
+    }
+    
+    public void updateInformation(String upName, int upGrade){
+       this.name = upName;
+       this.grade = upGrade;
+       System.out.println("Updated Information.");
+       
+    }
+}
+
